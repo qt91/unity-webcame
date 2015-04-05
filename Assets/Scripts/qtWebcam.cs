e0fe3f2f1da7021b4a8a66b5ee612e2c2d295b85
quangthinh <quangthinh.nguyen91@gmail.com>
2015-04-05 12:29:03 +0700

    first commit

---
+++ Assets/Scripts/qtWebcam.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class qtWebcam : MonoBehaviour {
+
+    WebCamTexture webCameraTexture;
+	void Start () {
+        webCameraTexture = new WebCamTexture();
+        renderer.material.mainTexture = webCameraTexture;
+        webCameraTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void TakePhoto()
+    {
+        //Khoi tao 1 Texture voi kich thuoc cua camera
+        Texture2D photo = new Texture2D(webCameraTexture.width, webCameraTexture.height);
+        //Lay cac diem anh cua camera gan vao Texture photo
+        photo.SetPixels(webCameraTexture.GetPixels());
+        //Thuc hien viec tren
+        photo.Apply();
+
+        //Chuyen doi thanh dinh dang phu hop
+        byte[] bytes = photo.EncodeToJPG();
+
+        //Ghi file
+        File.WriteAllBytes(Application.streamingAssetsPath+"photo.jpg",bytes);
+    }
+}
